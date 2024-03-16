e91197035df6a9eaee1a9c58b2da11c4d2470296
rraaniaa <raniachebbi87@example.com>
2024-03-16 14:57:13 +0100

    table peer class labo

--- src/main/java/com/example/clinique/Repositories/LipidiqueRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.clinique.Repositories;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-//public interface LipidiqueRepository extends JpaRepository<Lipidique, Long>
-//{
-
-//}
--- src/main/java/com/example/clinique/Repositories/TransaminasesRepository.java
+++
@@ -1,8 +0,0 @@
-package com.example.clinique.Repositories;
-
-import com.example.clinique.Entity.laboratoire.Transaminases;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface TransaminasesRepository extends JpaRepository<Transaminases, Long>
-{
-}
